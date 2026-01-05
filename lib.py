276cea23f74828826fb905c9111549db8966e87e
Lucachinou <lucachinou@duck.com>
2026-01-05 18:44:40 +0100

    All libs code

---
+++ lib.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+def get_shortcuts():
+    script = 'tell application "Shortcuts" to get name of every shortcut'
+    os.system(f"osascript -e '{script}'")
+
+def use_model(texte, shortcut: str="use_a_model"):
+    try:
+        processus = subprocess.run(
+            ["shortcuts", "run", shortcut],
+            input=str(texte),
+            text=True,
+            capture_output=True
+        )
+        if processus.returncode == 0:
+            if processus.stdout.startswith("{\\rtf"):
+                nettoyage = subprocess.run(
+                    ["textutil", "-convert", "txt", "-stdin", "-stdout"],
+                    input=processus.stdout,
+                    text=True,
+                    capture_output=True
+                )
+                return nettoyage.stdout.strip()
+            return processus.stdout.strip()
+    except Exception as e:
+        print(e)
